4a91a5eaf18ad14810ef64129957beeb618bff60
gkimeeq <gkimeeq@163.com>
2019-06-26 21:12:13 +0800

    Update multiprocessing_module.py

--- thread/multiprocessing_module.py
+++ thread/multiprocessing_module.py
@@ -58,3 +58,18 @@ print 'The number of cpu is ' + str(multiprocessing.cpu_count())
 for p in multiprocessing.active_children():
   print 'child p.name:', p.name, '\tp.id:', p.pid
 print 'End!!!'
+
+class ClockProcess(multiprocessing.Process):
+  def __init__(self, interval):
+    multiprocessing.Process.__init__(self)
+    self.interval = interval
+    
+  def run(self):
+    n = 5
+    while n > 0:
+      print 'The time is {0}'.format(time.ctime())
+      time.sleep(self.interval)
+      n -= 1
+      
+p = ClockProcess(3)
+p.start() # 自动调用run()
