64e53a71f6d9c58262e309f35033322944f157ae
gkimeeq <gkimeeq@163.com>
2019-06-26 21:20:32 +0800

    Update producer_consumer.py

--- thread/producer_consumer.py
+++ thread/producer_consumer.py
@@ -6,3 +6,23 @@ import multiprocessing
 Queue是多进程安全的队列，可以使用它来实现多进程之间的数据传递
 
 def writer_proc(q):
+  try:
+    q.put(1, block=False)
+  except:
+    pass
+  
+  
+def reader_proc(q):
+  try:
+    print q.get(block=False)
+  except:
+    pass
+  
+  
+q = multiprocessing.Queue()
+writer = multiprocessing.Process(target=writer_proc, args=(q,))
+writer.start()
+reader = multiprocessing.Process(target=reader_proc, args=(q,))
+reader.start()
+reader.join()
+writer.join()
